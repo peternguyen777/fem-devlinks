f359cc8087df3b2645e30febf3aea8fc383c15f8
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-09-27 15:58:10 +1000

    update globals.css with h3-h6 and p

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,37 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
+import localFont from "next/font/local";
+
+const InstrumentSans = localFont({
+  src: "../../public/fonts/InstrumentSans-VariableFont_wdth,wght.ttf",
+  variable: "--font-instrument",
+});
+
+const InstrumentSansItalic = localFont({
+  src: "../../public/fonts/InstrumentSans-Italic-VariableFont_wdth,wght.ttf",
+  variable: "--font-instrument-italic",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      {/* Turn this on if using shadcn */}
+      {/* <style jsx global>
+        {`
+          html {
+            font-family: ${InstrumentSans.style.fontFamily};
+          }
+        `}
+      </style> */}
+      <ClerkProvider {...pageProps}>
+        <main
+          className={`${InstrumentSansItalic.variable} ${InstrumentSans.variable}`}
+        >
+          <Component {...pageProps} />
+        </main>
+      </ClerkProvider>
+    </>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        instrument: ["var(--font-instrument)"],
+        "instrument-italic": ["var(--font-instrument-italic)"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
