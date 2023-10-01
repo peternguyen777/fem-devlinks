97ca07efe1e4c1d454746b54fea271ad2837e7e0
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-10-01 15:38:35 +1100

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import Image from "next/image";
 import { useRouter } from "next/router";
-import { Button } from "../ui/button";
+import LogoSmall from "./logo-small";
+import LogoLarge from "./logo-large";
+import LinksTab from "./links-tab";
+import ProfileTab from "./profile-tab";
+import PreviewButton from "./preview-button";
 import { UserButton } from "@clerk/nextjs";
 
 const Header = () => {
@@ -12,75 +14,15 @@ const Header = () => {
     <div className="sticky top-0 bg-[#FAFAFA] md:p-6">
       <div className="relative flex items-center justify-between rounded-b-xl bg-background py-4 pl-6 pr-4 shadow-lg md:rounded-xl">
         <>
-          <Image
-            priority
-            src="/images/logo-devlinks-small.svg"
-            alt="devlinks logo"
-            width={32}
-            height={32}
-            className="cursor-pointer md:hidden"
-            onClick={() => void router.push("/")}
-          />
-          <Image
-            priority
-            src="/images/logo-devlinks-large.svg"
-            alt="devlinks logo"
-            width={183}
-            height={40}
-            className="hidden cursor-pointer md:flex"
-            onClick={() => void router.push("/")}
-          />
+          <LogoSmall onClick={() => void router.push("/")} />
+          <LogoLarge onClick={() => void router.push("/")} />
         </>
         <div className="absolute left-1/2 flex -translate-x-1/2 md:relative md:left-auto md:translate-x-0">
-          <Button
-            variant="dlTab"
-            className={`px-[27px] py-[11px] ${
-              currentRoute === "/edit/links"
-                ? `bg-[#EFEBFF] text-[#633CFF]`
-                : `bg-background`
-            } md:gap-2`}
-          >
-            <Image
-              priority
-              src="/images/icon-links-header.svg"
-              alt="edit links"
-              width={20}
-              height={20}
-            />
-            <span className="hidden md:inline">Links</span>
-          </Button>
-          <Button
-            variant="dlTab"
-            className={`px-[27px] py-[11px] ${
-              currentRoute === "/edit/profile"
-                ? `bg-[#EFEBFF] text-[#633CFF]`
-                : `bg-background`
-            } md:gap-2`}
-          >
-            <Image
-              priority
-              src="/images/icon-profile-details-header.svg"
-              alt="edit profile"
-              width={21}
-              height={20}
-            />
-            <span className="hidden md:inline">Profile Details</span>
-          </Button>
+          <LinksTab currentRoute={currentRoute} />
+          <ProfileTab currentRoute={currentRoute} />
         </div>
-        <div className="flex items-center gap-4">
-          <Button variant="dlSecondary" className="px-4 py-[11px]">
-            <Image
-              priority
-              src="/images/icon-preview-header.svg"
-              alt="preview"
-              width={20}
-              height={20}
-              className="md:hidden"
-            />
-            <span className="hidden md:inline">Preview</span>
-          </Button>
-          <UserButton afterSignOutUrl="/" />
-        </div>
+        <PreviewButton />
+        <UserButton />
       </div>
     </div>
   );
---
+++ src/components/header/links-tab.tsx
@@ -0,0 +1,28 @@
+import { Button } from "../ui/button";
+
+const LinksTab = ({ currentRoute }: { currentRoute: string }) => (
+  <Button
+    variant="dlTab"
+    className={`group px-[27px] py-[11px] ${
+      currentRoute === "/edit/links"
+        ? `bg-[#EFEBFF] text-[#633CFF]`
+        : `bg-background`
+    } md:gap-2`}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="20"
+      fill="none"
+      viewBox="0 0 21 20"
+      className={`group-hover:fill-[#633CFF] ${
+        currentRoute === "/edit/links" ? `fill-[#633CFF]` : `fill-[#737373]`
+      }`}
+    >
+      <path d="M11.154 14.65a.936.936 0 0 1 0 1.329l-.464.464a4.689 4.689 0 1 1-6.631-6.631l1.884-1.884a4.687 4.687 0 0 1 6.432-.194.941.941 0 0 1-1.25 1.407 2.813 2.813 0 0 0-3.857.114l-1.883 1.882a2.813 2.813 0 1 0 3.978 3.978l.464-.464a.936.936 0 0 1 1.327 0ZM16.94 3.558a4.695 4.695 0 0 0-6.63 0l-.465.464a.94.94 0 1 0 1.328 1.328l.464-.464a2.813 2.813 0 0 1 3.978 3.978l-1.883 1.885a2.813 2.813 0 0 1-3.858.111.942.942 0 0 0-1.25 1.407 4.688 4.688 0 0 0 6.43-.19l1.884-1.884a4.695 4.695 0 0 0 .002-6.633v-.002Z" />
+    </svg>
+    <span className="hidden md:inline">Links</span>
+  </Button>
+);
+
+export default LinksTab;
