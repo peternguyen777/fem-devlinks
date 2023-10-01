18be4d476c68aede0dab24e765eb4d37e0472258
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-10-01 19:59:37 +1100

    reinstate example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { linksRouter } from "./routers/links";
+import { exampleRouter } from "./routers/example";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +8,7 @@ import { linksRouter } from "./routers/links";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  example: exampleRouter,
   links: linksRouter,
 });
 
