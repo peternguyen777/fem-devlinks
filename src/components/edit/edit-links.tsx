6dd8ff99252e0cf4a0d6d19f83a72af03fe4885a
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-10-03 17:42:42 +1100

    progress commit

---
+++ src/components/edit/edit-links.tsx
@@ -0,0 +1,26 @@
+import { api, type RouterOutputs } from "~/utils/api";
+import CustomizeLinks from "./customize-links";
+import PhonePreview from "./phone-preview";
+import { useEffect, useState } from "react";
+
+export type LinkState = RouterOutputs["links"]["getLinks"][number];
+
+const EditLinks = ({ userId }: { userId: string }) => {
+  const { data, isLoading } = api.links.getLinks.useQuery({ userId });
+  const [links, setLinks] = useState<LinkState[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setLinks(data);
+    }
+  }, [data]);
+
+  return (
+    <main className="bg-[#FAFAFA] p-4 md:p-6 md:pt-0 lg:flex lg:gap-6">
+      <PhonePreview />
+      <CustomizeLinks links={links} isLoading={isLoading} userId={userId} />
+    </main>
+  );
+};
+
+export default EditLinks;
