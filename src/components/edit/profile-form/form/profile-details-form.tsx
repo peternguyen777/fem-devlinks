ebe2ee03992bc53f23e54109eff4ca2b2f39ffa7
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-10-05 19:15:20 +1100

    refactor profile picture ui for tablet and desktop

--- src/components/edit/profile-form/form/profile-details-form.tsx
+++ src/components/edit/profile-form/form/profile-details-form.tsx
@@ -78,9 +78,9 @@ const ProfileDetailsForm = ({ profile }: { profile: Profile }) => {
           </p>
 
           <div className="my-6 flex-1 space-y-6 md:mb-10">
-            <div className="w-full rounded-lg bg-[#FAFAFA] p-5">
-              <p className="text-[#737373]">Profile picture</p>
-              <div className="mt-4 flex h-[193px] w-[193px] cursor-pointer flex-col items-center justify-center rounded-xl bg-[#EFEBFF]">
+            <div className="w-full rounded-lg bg-[#FAFAFA] p-5 md:flex md:items-center">
+              <p className="text-[#737373] md:w-[240px]">Profile picture</p>
+              <div className="mt-4 flex h-[193px] w-[193px] cursor-pointer flex-col items-center justify-center rounded-xl bg-[#EFEBFF] md:ml-4 md:mt-0 md:flex-none">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="40"
@@ -93,12 +93,12 @@ const ProfileDetailsForm = ({ profile }: { profile: Profile }) => {
                     d="M33.75 6.25H6.25a2.5 2.5 0 0 0-2.5 2.5v22.5a2.5 2.5 0 0 0 2.5 2.5h27.5a2.5 2.5 0 0 0 2.5-2.5V8.75a2.5 2.5 0 0 0-2.5-2.5Zm0 2.5v16.055l-4.073-4.072a2.5 2.5 0 0 0-3.536 0l-3.125 3.125-6.875-6.875a2.5 2.5 0 0 0-3.535 0L6.25 23.339V8.75h27.5ZM6.25 26.875l8.125-8.125 12.5 12.5H6.25v-4.375Zm27.5 4.375h-3.34l-5.624-5.625L27.91 22.5l5.839 5.84v2.91ZM22.5 15.625a1.875 1.875 0 1 1 3.75 0 1.875 1.875 0 0 1-3.75 0Z"
                   />
                 </svg>
-                <p className="mt-2 font-semibold text-[#633CFF]">
+                <p className="mt-2 font-semibold text-[#633CFF] ">
                   + Upload Image
                 </p>
               </div>
 
-              <h6 className="mt-6 text-[#737373]">
+              <h6 className="mt-6 text-[#737373] md:ml-6 md:mt-0">
                 Image must be below 1024x1024px. Use PNG or JPG format.
               </h6>
             </div>
