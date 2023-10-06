1328cd6fcddc7dc785f6cd1a22a9c68d38425b1e
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-10-06 23:17:31 +1100

    profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,16 +1,16 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import PreviewHeader from "~/components/header/preview/preview-header";
+import ProfileCard from "~/components/preview/profile-card";
 import { generateSSGHelper } from "~/server/api/helpers/ssgHelper";
 import { api } from "~/utils/api";
 
 const ProfilePage: NextPage<{ slug: string }> = ({ slug }) => {
   const { data, isLoading } = api.profile.getProfileBySlug.useQuery({ slug });
 
-  if (isLoading) return <div>Loading...</div>;
-
-  if (!data) return <div>404</div>;
+  if (isLoading || !data) {
+    return;
+  }
 
   return (
     <>
@@ -18,29 +18,7 @@ const ProfilePage: NextPage<{ slug: string }> = ({ slug }) => {
         <title>{`${data.firstName} ${data.lastName}'s Devlinks`}</title>
       </Head>
       <PreviewHeader data={data} />
-      <div>
-        <div className="relative h-36 bg-slate-600">
-          {data.image && (
-            <Image
-              src={data.image}
-              alt={`${data.firstName ?? ""}'s profile pic`}
-              width={128}
-              height={128}
-              className="left- absolute bottom-0 -mb-16 ml-4 rounded-full border-4 border-black bg-black"
-            />
-          )}
-        </div>
-        <div className="h-16 " />
-        <div className="p-4 text-2xl">{`${data.email}`}</div>
-        {data.links.map((item) => (
-          <div key={item.linkId}>
-            <p>{item.linkName}</p>
-            {/* <p>{item.url}</p> */}
-            <p>{item.priority}</p>
-          </div>
-        ))}
-        <div className="w-full border-b border-slate-400" />
-      </div>
+      <ProfileCard data={data} />
     </>
   );
 };
